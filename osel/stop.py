b368116f47a1d192766153375ecdeefb0e5636c0
Michael Wendland <accounts@michiwend.com>
2014-04-28 22:32:54 +0200

    adds api request and xml parsing to Stop class

--- osel/stop.py
+++ osel/stop.py
@@ -1,12 +1,18 @@
 from osel.departure_arrival import Arrival, Departure
+from osel.provider import EFAProvider
 
-import lxml
-import urllib
+from lxml import etree
+from urllib.request import urlopen
+from urllib.parse import urlencode
+
 
 class StopNotInCacheException(Exception): pass
 
+
 class StopFinderParser(object):
-    pass
+    def __init__(self, efa_provider):
+        self._provider = efa_provider
+
 
 class Stop(object):
     STOPCACHE = {}
@@ -19,16 +25,67 @@ class Stop(object):
         cls.STOPCACHE[stop.get_id()] = stop
 
     @staticmethod
-    def find_stops(search_string):
+    def find_stops(provider, search_string, max_results):
         """
-        Causes the library to search for a specific stop name
-        returns a list of Stops that were present in the response
-        from the EFA-server
+        Causes the library to search for a specific stop name. Returns a list of
+        stops that were present in the response from the EFA-server.
         """
-        pass # get shit from server -> urllib
-        # uebergebe shit an parser
-        # parser returnt adaequate objekte
-    
+        sf_params = {
+            'language'              : 'de',
+            'outputFormat'          : 'XML',
+            'useLocalityMainStop'   : 'true',
+            'locationServerActive'  : '1',
+            'type_sf'               : 'any',
+            'anyMaxSizeHitList'     : max_results,
+            'name_sf'               : search_string
+        }
+
+        stop_list = []
+
+        try:
+            response = urlopen(
+                            provider.get_api_base() + provider.get_sf_endpoint()
+                            + '?' + urlencode(sf_params)
+                        )
+            tree = etree.parse(response)
+            sf_root = tree.find('itdStopFinderRequest')
+            #print(etree.tostring(sf_root, pretty_print=True))
+
+            if etree.iselement(sf_root): # ToDo: check if odvNameElem is a child of sf_root
+                for xml_stop in sf_root.iter('odvNameElem'):
+                    stop_list.append(Stop(
+                                        xml_stop.text,
+                                        xml_stop.attrib['id'],
+                                        xml_stop.attrib['anyType'],
+                                        xml_stop.attrib['x'],
+                                        xml_stop.attrib['y'],
+                                        xml_stop.attrib['locality'],
+                                        xml_stop.attrib['objectName'],
+                                        xml_stop.attrib['buildingName'],
+                                        xml_stop.attrib['buildingNumber'],
+                                        xml_stop.attrib['postCode'],
+                                        xml_stop.attrib['streetName'],
+                                        xml_stop.attrib['posttown'],
+                                        xml_stop.attrib['value']
+                                    ))
+            else:
+                raise Exception('No valid stop finder response')
+
+        except IOError as e:
+            print(e)
+
+        except KeyError as e:
+            print('seems like an empty stop finder response: ' + str(e))
+
+        except etree.XMLSyntaxError as e:
+            print(e)
+
+        except Exception as e:
+            print(e)
+
+        return stop_list
+
+
     @classmethod
     def get_stop(cls, stop_id):
         """
@@ -38,24 +95,35 @@ class Stop(object):
             raise StopNotInCacheException()
         return cls.STOPCACHE[stop_id]
 
-    def __init__(self):
+    def __init__(self, name, stop_id, stop_type, lat=0, lon=0, locality='',
+                    object_name='', building_name='', building_number='',
+                    post_code='', street_name='', posttown='', value=''):
         """
-        trivial
+        trivials
         """
-        self._name = None
-        self._type = None
-        self._id = None
-        self._lon = None
-        self._lat = None
-        self._object = None
-        self._posttown = None
+        self._name = name
+        #self._listIndex="0"
+        #self._selected="0"
+        #self._matchQuality="885"
+        self._lat = lat
+        self._lon = lon
+        #self._mapName="NAV4"
+        self._id = stop_id
+        #self._omc="9772200"
+        #self._placeID="10"
+        self._type = stop_type
+        #self._anyTypeSort="1"
+        self._locality = locality
+        self._objectName = object_name
+        self._buildingName = building_name
+        self._buildingNumber = building_number
+        self._postCode = post_code
+        self._streetName = street_name
+        #self._nameKey=
+        self._posttown = posttown
+        self._value = value
 
-    def get_name(self):
-        return self._name
-    
-    def get_id(self):
-        return self._id
-    
+
     def set_name(self, name):
         self._name = name
 
@@ -67,3 +135,12 @@ class Stop(object):
 
     def get_arrivals(self, date):
         Arrival.get_arrivals(self, date)
+
+    def get_name(self):
+        return self._name
+
+    def get_id(self):
+        return self._id
+
+    def get_type(self):
+        return self._type
